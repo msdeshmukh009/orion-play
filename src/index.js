3d104d31be5a70b3ce96db98402839aafc5876d0
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-25 15:59:57 +0530

    initial setup done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./styles.css";
+import { App } from "./App";
 import { makeServer } from "./server";
 
 // Call make Server
