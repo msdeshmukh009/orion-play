6d352d62d8f1a3069a9f2990a6d04cda76c0440f
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-26 17:11:23 +0530

    refactor: stacked video item exported

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Footer } from "./footer/Footer";
 export { Sidebar } from "./sidebar/Sidebar";
 export { VideoCard } from "./video-card/VideoCard";
 export { PlaylistCard } from "./playlist-card/PlaylistCard";
+export { StackedVideoItem } from "./stacked-video-item/StackedVideoItem";
