e389551972acd068725888b301a036b574f94194
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-28 01:48:25 +0530

    feat: Routes component created

--- src/routes/NavigationRoutes.jsx
+++ src/routes/NavigationRoutes.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Explore, PlaylistListing } from "../pages";
+import { Explore, PlaylistListing, SingleVideo } from "../pages";
 
 const NavigationRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Explore />} />
       <Route path="/playlist" element={<PlaylistListing />} />
+      <Route path="/explore/:videoId" element={<SingleVideo />} />
     </Routes>
   );
 };
