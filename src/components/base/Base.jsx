45a03d502cc95191b0800038c492b97d75ba8bcb
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-28 02:47:22 +0530

    refactor: sidebar restriction added

--- src/components/base/Base.jsx
+++ src/components/base/Base.jsx
@@ -1,14 +1,22 @@
 import { Footer } from "../footer/Footer";
 import { Navbar } from "../navbar/Navbar";
 import { Sidebar } from "../sidebar/Sidebar";
+import { useLocation } from "react-router-dom";
+
 import "./base.css";
 
 const Base = ({ children }) => {
+  const { pathname } = useLocation();
+  const sidebarRestrictedRoutes = ["/signin", "/signup"];
   return (
     <>
       <Navbar />
-      <div className="main-container">
-        <Sidebar />
+      <div
+        className={
+          sidebarRestrictedRoutes.includes(pathname) ? "main-container-secondary" : "main-container"
+        }
+      >
+        {sidebarRestrictedRoutes.includes(pathname) ? null : <Sidebar />}
         <div className="children-container">{children}</div>
       </div>
       <Footer />
